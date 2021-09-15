43706399b9b492ee48f942176934a76510b7d7a5
MarekJurgens <marek.jurgens@ivkhk.ee>
2021-09-15 11:23:20 +0300

    SECOND COMMIT

--- src/pkg20_arrays/Main.java
+++ src/pkg20_arrays/Main.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package pkg20_arrays;
+import java.util.Random;
 
 /**
  *
@@ -15,7 +16,24 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int array[] = new int[20];
+        for (int i = 0; i < array.length; i++){
+            array[i] = (int)Math.floor(Math.random()*(10-0+1)+0);
+        }
+        int sum = 0;
+        
+        for (int i = 1; i < array.length-2; i++){
+            sum = sum + array[i];
+        }
+        System.out.println(sum);
+        
+        
+        
+        
+        
+        int RANDINT = (int)Math.floor(Math.random()*(10-0+1)+0);
+        System.out.print(RANDINT);
+        
     }
     
 }
